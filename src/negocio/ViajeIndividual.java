28d763c744b0c098ceab89fed5fae0c2f5c4060f
jedelgado <57530310+jedelgado@users.noreply.github.com>
2020-05-11 20:51:12 -0500

    mi primer Commit

---
+++ src/negocio/ViajeIndividual.java
@@ -0,0 +1,20 @@
+package negocio;
+
+import java.util.Date;
+
+
+public class ViajeIndividual extends Viaje{
+
+    public ViajeIndividual(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada){
+        
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+    }
+
+    @Override
+    public String descripcion(){
+        
+        return "Disfruta tu viaje individual";
+    }
+    
+    //No se sobreescribe cualquierMetodo2()
+}
